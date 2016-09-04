d15de8cdd38db68200136fae7101b4d78850f4a4
douglasbruce88 <doubru@Douglass-iMac.home>
2016-09-04 15:50:45 +0100

    Simple domain model with example

--- src/DomainModelling/DomainModelling.fs
+++ src/DomainModelling/DomainModelling.fs
@@ -5,10 +5,28 @@ module Domain =
 
   type Gender = Male | Female
 
-  type Person = { Name : string; Gender : Gender }
+  type Person = { Name : string; Gender : Gender; DateOfBirth : DateTime }
+
+  type ContributionFrequency = Monthly | Annual
+
+  type ContributionSchedule<[<Measure>] 'ccy> = {
+      Amount : decimal<'ccy>
+      Frequency : ContributionFrequency }
+  
+  type PensionPot<[<Measure>] 'ccy> = { 
+      Value : decimal<'ccy>; 
+      ValuationDate : DateTime; 
+      ContributionSchedule: ContributionSchedule<'ccy> }
 
   [<Measure>] type GBP
   [<Measure>] type USD
   [<Measure>] type EUR
-  
-  type PensionPot<[<Measure>] 'ccy> = { Value : decimal<'ccy> }
+
+  let valuePensionPot<[<Measure>] 'ccy> (pensionPot : PensionPot<'ccy>) (valuationDate: DateTime) = 
+     match pensionPot.ContributionSchedule.Frequency with 
+     | Monthly ->
+        let monthsInFuture = (valuationDate.Year - pensionPot.ValuationDate.Year) * 12 + valuationDate.Month - pensionPot.ValuationDate.Month
+        pensionPot.Value + decimal monthsInFuture * pensionPot.ContributionSchedule.Amount
+     | Annual ->
+        let yearsInFuture = valuationDate.Year - pensionPot.ValuationDate.Year
+        pensionPot.Value + decimal yearsInFuture * pensionPot.ContributionSchedule.Amount

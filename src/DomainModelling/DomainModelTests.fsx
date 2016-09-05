2979eb845434d6429e282f7ab3b443a60e57e747
Doug Bruce <douglasbruce88@gmail.comn>
2016-09-05 12:33:19 +0100

    Add files for Visual Studio running

--- src/DomainModelling/DomainModelTests.fsx
+++ src/DomainModelling/DomainModelTests.fsx
@@ -1,15 +1,24 @@
-  #load "DomainModelling.fs"
-  open DomainModelling.Domain
-  open System
-  
-  let me = { Name = "Doug"; Gender = Male; DateOfBirth = DateTime(1988, 01, 29) }
+#load "DomainModelling.fs"
 
-  let schedule = { Amount = 500m<GBP>; Frequency = Monthly} 
+open DomainModelling.Domain
+open System
 
-  let myPot = { Value = 10000m<GBP>; ValuationDate = DateTime(2016, 06, 30); ContributionSchedule = schedule }
+let me = 
+    { Name = "Doug"
+      Gender = Male
+      DateOfBirth = DateTime(1988, 01, 29) }
 
-  let valueMyPensionPot = valuePensionPot myPot
+let schedule = 
+    { Amount = 500m<GBP>
+      Frequency = Monthly }
 
-  valueMyPensionPot (DateTime(2017, 06, 30));;
-  valueMyPensionPot (DateTime(2026, 06, 30));;
-  valueMyPensionPot (DateTime(2046, 06, 30))
+let myPot = 
+    { Value = 10000m<GBP>
+      ValuationDate = DateTime(2016, 06, 30)
+      ContributionSchedule = schedule }
+
+let valueMyPensionPot = valuePensionPot myPot
+
+valueMyPensionPot (DateTime(2017, 06, 30))
+valueMyPensionPot (DateTime(2026, 06, 30))
+valueMyPensionPot (DateTime(2046, 06, 30))

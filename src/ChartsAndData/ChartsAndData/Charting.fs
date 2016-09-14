dbc70c4799e52d8d2dff0084883a5db4c6aeb69f
Doug Bruce <douglasbruce88@gmail.com>
2016-09-14 19:29:04 +0100

    Basic area chart of our pension pot valuation

---
+++ src/ChartsAndData/ChartsAndData/Charting.fs
@@ -0,0 +1,31 @@
+﻿namespace ChartsAndData
+
+module Charting = 
+    open Domain
+    open FSharp.Charting
+    open FSharp.Charting.ChartTypes
+    open System
+    open System.Windows.Forms
+    
+    let me = 
+        { Name = "Doug"
+          Gender = Male
+          DateOfBirth = DateTime(1988, 01, 29)
+          EmailAddress = None }
+    
+    let schedule = 
+        { Amount = 500m<GBP>
+          Frequency = Monthly }
+    
+    let myPot = 
+        { Value = 10000m<GBP>
+          ValuationDate = DateTime(2016, 06, 30)
+          ContributionSchedule = schedule }
+    
+    let valueMyPot = 
+        [ for i in 1..50 -> 
+              let date = myPot.ValuationDate.AddYears(i)
+              (date, valuePensionPot myPot date) ]
+    
+    let testChart = Chart.Area(valueMyPot)
+    let chartControl = new ChartControl(testChart, Dock = DockStyle.Fill, Name = "Valuation")
--- src/ChartsAndData/ChartsAndData/Form.fs
+++ src/ChartsAndData/ChartsAndData/Form.fs
@@ -3,5 +3,13 @@
 [<AutoOpen>]
 module Form = 
     open System.Windows.Forms
-    let form = new Form()
+    open System.Drawing
+
+    let form = new Form(Name = "Main", Visible = true, Text = "Tickers", AutoScaleDimensions = SizeF(6.F, 13.F), AutoScaleMode = AutoScaleMode.Font, ClientSize = Size(1000, 600))
+
+    let panel = new Panel(Location = new Point(0, 0), Size = new Size(800, 600), BorderStyle = BorderStyle.Fixed3D, Dock = DockStyle.Fill)
+    panel.Controls.Add Charting.chartControl
+
+    form.Controls.Add panel
+
     let load() = form

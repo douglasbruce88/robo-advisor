4553315dea9024f55b5c9891c155a4424ebd5022
Doug Bruce <douglasbruce88@gmail.com>
2016-09-18 15:41:39 +0100

    Test for State Pension Age calc

---
+++ src/ChartsAndData/ChartsAndData.Tests/StatePensionAgeTests.fs
@@ -0,0 +1,19 @@
+﻿namespace ChartsAndData.Tests
+
+module StatePensionAgeTests = 
+    open NUnit.Framework
+    open Swensen.Unquote
+    open ChartsAndData.StatePensionAge
+    open System
+    
+    let testCaseSource = 
+       [ DateTime(1988, 01, 29), DateTime(2056, 01, 29)
+         DateTime(1954, 05, 07), DateTime(2020, 01, 06)
+         DateTime(1960, 07, 09), DateTime(2026, 11, 09)
+       ] |> List.map (fun (input, result) -> TestCaseData(input, result))
+
+    [<Test>]
+    [<TestCaseSource("testCaseSource")>]
+    let ``SPA Calculation is correct``(input: DateTime, expected: DateTime) = 
+        let actual = input |> calcSPA 
+        actual =! expected
--- src/ChartsAndData/ChartsAndData/StatePensionAge.fs
+++ src/ChartsAndData/ChartsAndData/StatePensionAge.fs
@@ -4,7 +4,8 @@ module StatePensionAge =
     open FSharp.Data
     open System
     
-    let SPAData = CsvProvider<"StatePensionAge.csv", PreferOptionals=true>.GetSample()
+    let SPAData = 
+        CsvProvider<"StatePensionAge.csv", PreferOptionals=true, Culture="en-gb">.GetSample()
     
     let calcSPA (birthDate : DateTime) = 
         let defaultRetirementAge = 68
@@ -15,13 +16,14 @@ module StatePensionAge =
                 match Int32.TryParse(str.Substring(0, 2)) with
                 | (true, i) -> 
                     let dateWithYearsAdded = birthDate.AddYears(i)
-                    match Int32.TryParse(str.Split([| "and " |], StringSplitOptions.None).[0].Substring(0, 2)) with
-                    | (true, j) -> dateWithYearsAdded.AddMonths(j)
-                    | _ -> dateWithYearsAdded
+                    let split = str.Split([| " and " |], StringSplitOptions.None)
+                    match split.Length with
+                    | 1 -> dateWithYearsAdded
+                    | _ -> dateWithYearsAdded.AddMonths(Int32.Parse(split.[1].Substring(0, 2)))
                 | _ -> birthDate.AddYears(defaultRetirementAge)
             | None -> birthDate.AddYears(defaultRetirementAge)
         
-        let getDataRow = SPAData.Rows |> Seq.findBack (fun x -> x.``Born After`` > birthDate)
+        let getDataRow = SPAData.Rows |> Seq.findBack (fun x -> birthDate > x.``Born After``)
         match getDataRow.``Date State Pension age reached`` with
         | Some date -> date
         | None -> processAgeColumn getDataRow.``Age State Pension age reached``
